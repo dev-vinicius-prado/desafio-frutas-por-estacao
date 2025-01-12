461e3e0b10eb7418850afa90b410194959dcc61b
Vinicius Prado <developer.vinicius.prado@gmail.com>
2025-01-12 16:30:18 -0300

    feature(Desafio frutas por estação): Dados do usuário
    - implementa a estrutura que captura a entrada de dados do usuário;

--- src/main/java/br/com/devviniciusprado/Main.java
+++ src/main/java/br/com/devviniciusprado/Main.java
@@ -1,17 +1,30 @@
 package br.com.devviniciusprado;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
+import static java.util.Objects.isNull;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        var scanner = new Scanner(System.in);
+        System.out.println("Digite a lista de frutas separadas por espaco:");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        final String frutas = scanner.nextLine();
+        if (isNull(frutas) || frutas.trim().isEmpty()) {
+            throw new IllegalArgumentException("Informe pelo menos 1 fruta");
         }
+
+        List<String> listaFrutas = processarEntradaDoUsuario(frutas);
+
+        Map<String, Collection<String>> frutasPorEstacao = new ClassificadorFrutasPorEstacao().classificar(listaFrutas);
+        System.out.println("::Frutas por estação::");
+        System.out.println(frutasPorEstacao);
+    }
+
+    private static List<String> processarEntradaDoUsuario(String frutas) {
+        String[] arrayFrutas = frutas.replaceAll(",", " ").split(" ");
+        return Arrays.stream(arrayFrutas)
+                .map(String::trim)
+                .toList();
     }
 }
